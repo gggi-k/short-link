3f21ac5b1030ed97ebded8f7d7444744971b834e
jgram <jprgr16@gmail.com>
2022-10-17 21:26:07 +0900

    feat: short link 기능

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # short-link
+
+
+## 스킬
+* Java11
+* Spring Boot
+* JPA
+* Hibernate
+* Spring Data JPA
+* H2
+* Bean Validation
+* Hateoas
+* Mapstruct
+* Junit
+* Mockito
+
+## 엔티티
+
+## 기능확인
+
+
+## 테스트확인
--- src/main/java/kr/project/shortlink/api/repository/ShortLinkRepository.java
+++ src/main/java/kr/project/shortlink/api/repository/ShortLinkRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 
 public interface ShortLinkRepository extends JpaRepository<ShortLinkEntity, Long> {
 
-    <T> T findByShortId(String shortId, Class<T> type);
-
     Optional<ShortLinkEntity> findByUri(URL uri);
 }
